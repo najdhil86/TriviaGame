6ce7007695f901d3455949fb050d374c2ce6b413
Naj Dhillon <najdhil86@gmail.com>
2019-07-24 10:45:53 -0700

    Fixed center issue with game. Fix scoring when the user does not select anything. Cleaned up Javascript to be cleaner.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,117 +1,146 @@
-  //Showing and hiding the three main sections: Start Content, Main Content, and Overall Results Content
+var counter = 120;
+var correctans = 0;
+var incorrectans = 0;
+var unansweredans = 0;
+
+//Setting correct answers, incorrect answers, and unanswered answers variables
+
+var rightAns = $('#right').text(correctans);
+var wrongAns = $('#wrong').text(incorrectans);
+var unAns = $('#unans').text(unansweredans);
+
+
+var answer1 = $("input[name='lebron']:checked").val();
+var answer2 = $("input[name='wiggins']:checked").val();
+var answer3 = $("input[name='champ']:checked").val();
+var answer4 = $("input[name='finals']:checked").val();
+var answer5 = $("input[name='ming']:checked").val();
+var answer6 = $("input[name='magic']:checked").val();
+var answer7 = $("input[name='mostwins']:checked").val();
+
+//Showing and hiding the three main sections: Start Content, Main Content, and Overall Results Content
 
 $('#mainform').hide();
 $('#startbutton').show();
 $('#overallresults').hide();
+
+function decreaseTimer(){
+    
+  counter -= 1;
+  $('#timer').text(counter);
+
+  if (counter == 0){
+
+    answer1 = $("input[name='lebron']:checked").val();
+    answer2 = $("input[name='wiggins']:checked").val();
+    answer3 = $("input[name='champ']:checked").val();
+    answer4 = $("input[name='finals']:checked").val();
+    answer5 = $("input[name='ming']:checked").val();
+    answer6 = $("input[name='magic']:checked").val();
+    answer7 = $("input[name='mostwins']:checked").val();
+    
+    isItCorrect(answer1);
+    isItCorrect(answer2);
+    isItCorrect(answer3);
+    isItCorrect(answer4);
+    isItCorrect(answer5);
+    isItCorrect(answer6);
+    isItCorrect(answer7);
+
+    $("input[type='submit']").attr('disabled', true);
+    clearInterval(timer);
+
+    $('#mainform').hide();
+    $('#overallresults').show();
+  }
+
+}
+
+function isItCorrect(a) {
   
+  if (a == 'correct' ) {
 
-   $('#start').on('click', function(){
-
-    $('#mainform').show();
-    
-    $('#startbutton').hide();
-
-    $('#overallresults').hide();
-
-
-    var counter = 10;
+  correctans ++;
+  console.log("Correct value " + correctans);
+  rightAns = $('#right').text(correctans);
   
-    $('#timer').text(counter);
+  } else if (a == undefined) {
+
+    unansweredans ++;
+    incorrectans++;
+    unAns = $('#unans').text(unansweredans);
+    wrongAns = $('#wrong').text(incorrectans);
+
+  } else  {
+
+    incorrectans ++;
+    console.log("Incorrect value " + incorrectans);
+    wrongAns = $('#wrong').text(incorrectans);
+    // debugger;
+  }
+}
+
   
-    function decreaseTimer(){
+//Start button functionality
+
+$('#start').on('click', function(){
+
+  //Showing and hiding divs
+
+  $('#mainform').show();
     
-      counter -= 1;
-      $('#timer').text(counter);
+  $('#startbutton').hide();
 
-      if (counter == 0){
+  $('#overallresults').hide();
+  
+  $('#timer').text(counter);
+  
+  
+  //Function to decrease the counter(timer)
+  decreaseTimer();
 
-      $("input[type='submit']").attr('disabled', true);
-      clearInterval(timer);
-
-      $('#mainform').hide();
-      $('#overallresults').show();
-
-      }
-    
-    }
-
-    var timer = setInterval(decreaseTimer, 1*1000);
-    
-    $('#timer').text(counter);
+  var timer = setInterval(decreaseTimer, 1*1000);
+  
+  //Display timer
+  $('#timer').text(counter);
 
     
   })
 
-    //Setting correct answers, incorrect answers, and unanswered answers variables
+//Submit button functionality. What happens when you press the submit button
+$("input[type='submit']").on('click', function(){
 
-    var correctans = 0;
-    var rightAns = $('#right').text(correctans);
-
-    var incorrectans = 0;
-    var wrongAns = $('#wrong').text(incorrectans);
     
-    var unansweredans = 0;
-    var unAns = $('#unans').text(unansweredans);
-  //Submit button functionality. What happens when you press the submit button
+  rightAns = $('#right').text(correctans);
+  wrongAns = $('#wrong').text(incorrectans);
 
-  $("input[type='submit']").on('click', function(){
 
-    var correctans = 0;
-    var rightAns = $('#right').text(correctans);
+  // Setting Variable for answer from all questions
     
+  answer1 = $("input[name='lebron']:checked").val();
+  answer2 = $("input[name='wiggins']:checked").val();
+  answer3 = $("input[name='champ']:checked").val();
+  answer4 = $("input[name='finals']:checked").val();
+  answer5 = $("input[name='ming']:checked").val();
+  answer6 = $("input[name='magic']:checked").val();
+  answer7 = $("input[name='mostwins']:checked").val();
 
-    var incorrectans = 0;
-    var wrongAns = $('#wrong').text(incorrectans);
+  //Checking what are the right answers
 
+  isItCorrect(answer1);
+  isItCorrect(answer2);
+  isItCorrect(answer3);
+  isItCorrect(answer4);
+  isItCorrect(answer5);
+  isItCorrect(answer6);
+  isItCorrect(answer7);
 
-    // Setting Variable for answer from all questions
+  //Show Overall results, Hide the two other sections
     
-    var answer1 = $("input[name='lebron']:checked").val();
+  $('#mainform').hide();
+  $('#overallresults').show();  
 
-    var answer2 = $("input[name='wiggins']:checked").val();
-
-    var answer3 = $("input[name='champ']:checked").val();
-
-    var cAnsDisplay = $('')
-    
-    function isItCorrect(a) {
-
-      if (a == 'correct' ) {
-
-      correctans ++;
-      console.log("Correct value " + correctans);
-      rightAns = $('#right').text(correctans);
-      //  debugger;
-      } else if (a == undefined) {
-
-        unansweredans ++;
-        incorrectans++;
-        unAns = $('#unans').text(unansweredans);
-        wrongAns = $('#wrong').text(incorrectans);
-
-      } else  {
-
-        incorrectans ++;
-        console.log("Incorrect value " + incorrectans);
-        wrongAns = $('#wrong').text(incorrectans);
-        // debugger;
-      }
-
-      
-    }
-
-    //Checking what are the right answers
-
-    isItCorrect(answer1);
-    isItCorrect(answer2);
-    isItCorrect(answer3);
-
-    //Show Overall results, Hide the two other sections
-    
-    // $('#mainform').hide();
-    // $('#overallresults').show();  
-
-    event.preventDefault();
+  event.preventDefault();
     
   })
 
